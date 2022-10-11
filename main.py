967864fc9e202a96a095819180b22b2b1bcba66c
AlexandrBaranov7 <petrov_i_l@mail.ru>
2022-10-11 19:43:26 +0500

    msg

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.app import App
 
 class MainApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, App!", halign="center")
+        return MDLabel(text="Hello, MApp!", halign="center")
 
 
 MainApp().run()
